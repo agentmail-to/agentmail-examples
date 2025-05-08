37cf6fd681956e2ade28e8da6f81146f15f69c60
Haakam Aujla <haakamsingh21@gmail.com>
2025-05-08 04:50:49 -0400

    Improvements

--- email-agent/main.py
+++ email-agent/main.py
@@ -1,28 +1,34 @@
 import os
-import json
 import asyncio
 from threading import Thread
 
 import ngrok
 from flask import Flask, request, Response
 
+from agentmail import AgentMail
 from agentmail_toolkit.openai import AgentMailToolkit
 from agents import Agent, Runner
 
 
 port = 8080
 domain = os.getenv("WEBHOOK_DOMAIN")
-inbox = f"{os.getenv('INBOX_USERNAME')}@agentmail.io"
+inbox = f"{os.getenv('INBOX_USERNAME')}@agentmail.to"
 
 listener = ngrok.forward(port, domain=domain, authtoken_from_env=True)
 app = Flask(__name__)
 
+client = AgentMail()
+
+instructions = f"""
+You are an email agent. Your name is AgentMail. Your email address is {inbox}.
+Respond as if you are writing an email. Do not include the subject, only the body.
+"""
+
 agent = Agent(
     name="Email Agent",
-    instructions=f"You are an email agent created by AgentMail. Your email address is {inbox}. Reply to the user's message as you deem fit. Get the thread for context. Do not ask for more information, just reply to the user's message at all costs.",
-    tools=AgentMailToolkit().get_tools(),
+    instructions=instructions,
+    tools=AgentMailToolkit(client).get_tools(),
 )
-
 
 @app.route("/webhooks", methods=["POST"])
 def receive_webhook():
@@ -31,15 +37,22 @@ def receive_webhook():
 
 
 def process_webhook(payload):
-    prompt = json.dumps(payload, indent=4)
+    message = payload["message"]
+
+    prompt = f"""
+From: {message["from"]}
+Subject: {message["subject"]}
+Body:\n{message["text"]}
+"""
     print("Prompt:\n\n", prompt, "\n")
 
     response = asyncio.run(Runner.run(agent, prompt))
     print("Response:\n\n", response.final_output, "\n")
 
+    client.messages.reply(inbox_id=inbox, message_id=message["message_id"], text=response.final_output)
+
 
 if __name__ == "__main__":
-    print(f"Ingress: {listener.url()}")
     print(f"Inbox: {inbox}\n")
 
     app.run(port=port)
